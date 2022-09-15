9385e308ea70d1963a5c9a179d68156d3b9fa0a2
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-16 00:08:31 +0200

    implement rule based matcher

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from datetime import datetime
 from src.data_handler import create_dataset, process_data
 from src.majority_class_model import evaluate_majority_class
+from src.rule_based_matching.rule_loader import RuleMatcher
+import sklearn.metrics
 
 
 def main():
@@ -17,6 +19,18 @@ def main():
     train_dataset, test_dataset = process_data(dataset)
 
     evaluate_majority_class(test_dataset)
+
+    rule_matcher = RuleMatcher()
+    test_input = test_dataset.map(lambda _input, _: _input).as_numpy_iterator()
+
+    test_labels = list(test_dataset.map(lambda _, label: label).as_numpy_iterator())
+    test_labels = [label.decode("utf-8") for label in test_labels]
+
+    predicted_labels = rule_matcher.predict(test_input)
+
+    logging.info("Classification Report for Rule Based Matching")
+    logging.info("\n" + sklearn.metrics.classification_report(test_labels, predicted_labels))
+
 
 if __name__ == '__main__':
     main()
---
+++ src/rule_based_matching/rule_matcher.py
@@ -0,0 +1,50 @@
+from collections.abc import Iterable
+import csv
+import os
+import re
+from typing import Final, Union
+
+from tensorflow.python.util.lazy_loader import logging
+
+from src.majority_class_model import score_input_majority_class
+
+dirname = os.path.dirname(__file__)
+
+
+class RuleMatcher:
+    WHITESPACE_REGEX: Final = re.compile(r"\s")
+    LETTER_SUB_REGEX: Final = re.compile(r"[^a-zA-z]")
+
+    def __init__(self):
+        self.rules = {}
+
+        self.load_rules()
+
+    def load_rules(self):
+        csv_path = os.path.join(dirname, "rules.csv")
+
+        with open(csv_path, encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for line in reader:
+                for label, rule in line.items():
+                    if not rule:
+                        continue
+
+                    self.rules[rule] = label
+
+    def classify(self, input_text: Union[bytes, str]) -> str:
+        if isinstance(input_text, bytes):
+            input_text = input_text.decode("utf-8")
+
+        tokens = re.split(self.WHITESPACE_REGEX, input_text)
+        words = [re.sub(self.LETTER_SUB_REGEX, "", token) for token in tokens]
+
+        for word in words:
+            if label := self.rules.get(word):
+                logging.info(f"{label=} for {word=}")
+                return label
+
+        return score_input_majority_class(input_text)
+
+    def predict(self, features: Iterable):
+        return [self.classify(feature) for feature in features]
