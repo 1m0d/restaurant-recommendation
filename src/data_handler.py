425842f4860cae8bd3cfd9d07bb60ec7b5efad90
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-15 19:24:55 +0200

    move data handling to its own file

---
+++ src/data_handler.py
@@ -0,0 +1,42 @@
+from typing import Final
+
+import tensorflow as tf
+
+DATASET_SIZE: Final = 25501
+
+SEED = 42
+TRAIN_SPLIT = 0.85
+
+
+def create_dataset(path: str):
+    dataset = tf.data.TextLineDataset(
+        path,
+        compression_type=None,
+        buffer_size=None,
+        num_parallel_reads=None,
+        name=None,
+    )
+
+    def separate_data(line):
+        split_line = tf.strings.split(line, " ", maxsplit=1)
+        label = split_line[0]
+        input_text = split_line[1]
+        return input_text, label
+
+    return dataset.map(separate_data)
+
+
+def _lowercase_input(input_text, label):
+    return tf.strings.lower(input_text), label
+
+
+def process_data(dataset):
+    dataset = dataset.map(_lowercase_input)
+    dataset = dataset.shuffle(DATASET_SIZE, seed=SEED)
+
+    train_dataset_size = int(TRAIN_SPLIT * DATASET_SIZE)
+    test_dataset_size = DATASET_SIZE - train_dataset_size
+    train_dataset = dataset.take(train_dataset_size)
+    test_dataset = dataset.skip(train_dataset_size).take(test_dataset_size)
+
+    return train_dataset, test_dataset
--- src/main.py
+++ src/main.py
@@ -1,43 +1,4 @@
-from typing import Final
-import tensorflow as tf
-
-
-DATASET_SIZE: Final = 25501
-
-SEED: Final = 42
-TRAIN_SPLIT: Final = 0.85
-
-
-def load_data(path: str):
-    dataset = tf.data.TextLineDataset(
-        path,
-        compression_type=None,
-        buffer_size=None,
-        num_parallel_reads=None,
-        name=None
-    )
-
-    def separate_data(line):
-        split_line = tf.strings.split(line, " ", maxsplit=1)
-        label = split_line[0]
-        input_text = split_line[1]
-        return input_text, label
-
-    return dataset.map(separate_data)
-
-def lowercase_input(input_text, label):
-    return tf.strings.lower(input_text), label
-
-def process_data(dataset):
-    dataset = dataset.map(lowercase_input)
-    dataset = dataset.shuffle(DATASET_SIZE, seed=SEED)
-
-    train_dataset_size = int(TRAIN_SPLIT * DATASET_SIZE)
-    test_dataset_size = DATASET_SIZE - train_dataset_size
-    train_dataset = dataset.take(train_dataset_size)
-    test_dataset = dataset.skip(train_dataset_size).take(test_dataset_size)
-
-    return train_dataset, test_dataset
+from src.data_handler import create_dataset, process_data
 
 
 def score_input_majority_class(_input_text):
@@ -55,11 +16,10 @@ def evaluate_majority_class(test_dataset):
         else:
             incorrect += 1
 
-
     accuracy = correct / (correct + incorrect)
     print(f"Accuracy for majority class model: {(accuracy * 100):.2f}%")
 
 
-dataset = load_data("../dialog_acts(1).dat")
+dataset = create_dataset("../dialog_acts(1).dat")
 train_dataset, test_dataset = process_data(dataset)
 evaluate_majority_class(test_dataset)
