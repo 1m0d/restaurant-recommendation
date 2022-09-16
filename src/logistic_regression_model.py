fc3d3b1a03c0b2c50152920a11b06d9db9db75f4
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-16 11:11:04 +0200

    fix typo

--- src/logistic_regression_model.py
+++ src/logistic_regression_model.py
@@ -3,10 +3,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
 class LogisticRegressionModel:
-    def __init__(self, train_dataset: Iterable):
+    def __init__(self, train_inputs: Iterable):
         self.model = LogisticRegression()
         self.count_vectorizer = CountVectorizer()
-        self.count_vectorizer.fit(train_dataset)
+        self.count_vectorizer.fit(train_inputs)
 
     def feature_extraction(self, dataset: Iterable):
         return self.count_vectorizer.transform(dataset)
