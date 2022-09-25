e3e7a3ced7f5f3da5c7373ce9a3391151b63d638
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-26 00:53:16 +0200

    extract input classification, fix method call

--- src/restaurant_recommender.py
+++ src/restaurant_recommender.py
@@ -2,6 +2,7 @@ import logging
 from typing import Iterable, Tuple
 
 import pandas as pd
+from src.dialog_handler import DialogHandler
 
 from src.preferences import Preferences
 from src.state_manager import StateManager
@@ -9,7 +10,7 @@ from src.state_manager import StateManager
 
 class RestaurantRecommender:
     def __init__(self, classifier, csv_path: str = "./restaurant_info.csv"):
-        self.state_manager = StateManager("")
+        self.state_manager = StateManager()
         self.preferences = Preferences()
         self.classifier = classifier
         self.recommend_restaurants: Iterable[str] = []
@@ -21,30 +22,30 @@ class RestaurantRecommender:
 
         while True:
             user_input = input().lower()
-            feature_vector = self.classifier.feature_extraction([user_input])
-            trigger = self.classifier.model.predict(feature_vector)[0].decode("utf-8")
-            print(f"{trigger=}")
-
+            trigger = self._input_to_trigger(user_input=user_input)
             if trigger == "inform":
                 matched_preferences, levenshtein_used = keyword_match(user_input)
                 self.preferences += matched_preferences
+                self.state_manager.current_preferences = self.preferences
 
                 if levenshtein_used:
                     trigger = "inform_unknown"
                 else:
                     trigger = "inform_known"
 
-                    if self.preferences.is_full:
+                    if self.preferences.is_full():
                         self._find_restaurants()
                         trigger = "restaurant_recommend"
 
-            trigger_func = getattr(self.state_manager, trigger)
+            self.logger.debug(f"{trigger=}")
+
+            trigger_func = getattr(self.state_manager, trigger, None)
             if not trigger_func:
                 self.logger.error(
-                    f"No state transition trigger found for {trigger_func=}. Defaulting"
+                    f"No state transition trigger found for {trigger=}. Defaulting"
                     " to null."
                 )
-                trigger_func = self.state_manager.null
+                trigger_func = self.state_manager.machine.null
             trigger_func()
 
             self._state_to_utterance()
@@ -52,8 +53,12 @@ class RestaurantRecommender:
             if self.state_manager.state == "final":
                 break
 
-    def _state_to_utterance(self) -> str:
-        pass
+    def _input_to_trigger(self, user_input) -> str:
+        feature_vector = self.classifier.feature_extraction([user_input])
+        trigger = self.classifier.model.predict(feature_vector)[0].decode("utf-8")
+        self.logger.debug(f"classified input as {trigger}")
+
+        return trigger
 
     def _find_restaurants(self):
         self.recommend_restaurants = self.restaurants.query(
