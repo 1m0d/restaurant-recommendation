daa55d959f92c334a0e5658942d0adec7675c8ad
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-25 22:43:16 +0200

    implement restaurant finding from csv, call state transition trigger

--- src/restaurant_recommender.py
+++ src/restaurant_recommender.py
@@ -1,13 +1,18 @@
+import pandas as pd
+from typing import Iterable, List, Tuple
 from src.state_manager import StateManager
 from src.preferences import Preferences
+import logging
 
 class RestaurantRecommender:
-    def __init__(self, classifier):
+    def __init__(self, classifier, csv_path: str = "./restaurant_info.csv"):
         self.state_manager = StateManager("")
         self.preferences = Preferences()
         self.classifier = classifier
-        self.matched_preferences = []  #  TODO: remove in the end if turns out not needed
-        self.recommend_restaurants = []
+        self.matched_preferences: List[Preferences] = []  #  TODO: remove in the end if turns out not needed
+        self.recommend_restaurants: Iterable[str] = []
+        self.restaurants = pd.read_csv(csv_path)
+        self.logger = logging.getLogger(__name__)
 
     def run(self):
         print("Hello, how can I help you?")
@@ -15,35 +20,43 @@ class RestaurantRecommender:
         while True:
             user_input = input().lower()
             feature_vector = self.classifier.feature_extraction([user_input])
-            predicted_act = self.classifier.model.predict(feature_vector)[0].decode("utf-8")
-            print(f"{predicted_act=}")
+            trigger = self.classifier.model.predict(feature_vector)[0].decode("utf-8")
+            print(f"{trigger=}")
 
-            if predicted_act == "inform":
+            if trigger == "inform":
                 matched_preferences, levenshtein_used = keyword_match(user_input)
-                self.matched_preferences.append(matches)
+                self.matched_preferences.append(matched_preferences)
                 self.preferences += matched_preferences
 
                 if levenshtein_used:
-                    predicted_act = "inform_unknown"
+                    trigger = "inform_unknown"
                 else:
+                    trigger = "inform_known"
+
                     if self.preferences.is_full:
-                        self.recommend_restaurants.append(recommend_restaurant())
-                        self.state_manager.call("restaurant_recommended")
+                        self._find_restaurants()
+                        trigger = "restaurant_recommend"
 
-                    predicted_act = "inform_known"
+            trigger_func = getattr(self.state_manager, trigger)
+            if not trigger_func:
+                self.logger.error(f"No state transition trigger found for {trigger_func=}. Defaulting to null.")
+                trigger_func = self.state_manager.null
+            trigger_func()
 
-            self.state_manager.call(predicted_act)
-            self.state_to_utterance(self.state_manager.state, self.preference)
+            self._state_to_utterance()
 
             if self.state_manager.state == "final":
                 break
 
-    def state_to_utterance(self) -> str:
+    def _state_to_utterance(self) -> str:
         pass
 
-def keyword_match(input_text) -> (Preferences, bool):
-    pass
 
-def recommend_restaurant(preferences) -> str:
-    #  print()
-    pass
+    def _find_restaurants(self):
+        self.recommend_restaurants = self.restaurants.query(self.preferences.to_pandas_query())
+
+
+def keyword_match(input_text) -> Tuple[Preferences, bool]:
+    return Preferences(), False
+
+
