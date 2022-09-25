97337af9b44d7d5337d36f677d77d84f814b090b
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-26 00:52:09 +0200

    fix methods in Preferences class

--- src/preferences.py
+++ src/preferences.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Final, List, Optional
+from typing import ClassVar, List, Optional
 
 
 @dataclass
@@ -8,7 +8,7 @@ class Preferences:
     area: Optional[str] = None
     price_range: Optional[str] = None
 
-    CSV_ALIASES: Final = {
+    CSV_ALIASES: ClassVar = {
         "food_type": "food",
         "area": "area",
         "price_range": "pricerange",
@@ -18,13 +18,15 @@ class Preferences:
         return [field_name for field_name, value in self.__dict__.items() if not value]
 
     def is_full(self) -> bool:
-        return all(self.__dict__)
+        return all(self.__dict__.values())
 
-    def __add__(self, other):
+    def __iadd__(self, other):
         """Merges preferences, if value set in both instances {other} has preference"""
         for key, value in other.__dict__.items():
             if value:
                 setattr(self, key, value)
+
+        return self
 
     def to_pandas_query(self) -> str:
         return " & ".join(
