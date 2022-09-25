3c204dc602ad8112b962715178fac9d47e68f654
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-25 22:42:29 +0200

    extend Preferences class with merging and pandas query conversion

--- src/preferences.py
+++ src/preferences.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Final, List, Optional
 
 @dataclass
 class Preferences:
     food_type: Optional[str] = None
     area: Optional[str] = None
     price_range: Optional[str] = None
+
+    CSV_ALIASES: Final = {
+        "food_type": "food",
+        "area": "area",
+        "price_range": "pricerange"
+    }
 
     def missing_preferences(self) -> List:
         return [field_name for field_name, value in self.__dict__.items() if not value]
@@ -14,4 +20,10 @@ class Preferences:
         return all(self.__dict__)
 
     def __add__(self, other):
-        """merge preferences"""
+        """ Merges preferences, if value set in both instances {other} has preference """
+        for key, value in other.__dict__.items():
+            if value:
+                setattr(self, key, value)
+
+    def to_pandas_query(self) -> str:
+        return " & ".join([f"{self.CSV_ALIASES[key]} == {value}" for key, value in self.__dict__.items()])
