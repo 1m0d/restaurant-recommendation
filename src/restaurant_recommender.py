11c36b4920622ef099bc1617c105115b4a1d9728
Domonkos Debreczeni <domonkos.debreczeni@sophos.com>
2022-09-27 16:17:30 +0200

    use new keyword matcher, handle restaurant suggesting

--- src/restaurant_recommender.py
+++ src/restaurant_recommender.py
@@ -1,8 +1,9 @@
 import logging
-from typing import Iterable, Tuple
+from typing import Iterable
 
 import pandas as pd
 from src.dialog_handler import DialogHandler
+from src.keyword_matching import KeywordMatcher
 
 from src.preferences import Preferences
 from src.state_manager import StateManager
@@ -12,6 +13,7 @@ class RestaurantRecommender:
     def __init__(self, classifier, csv_path: str = "./restaurant_info.csv"):
         self.state_manager = StateManager()
         self.preferences = Preferences()
+        self.keyword_matcher = KeywordMatcher()
         self.classifier = classifier
         self.recommend_restaurants: Iterable[str] = []
         self.restaurants = pd.read_csv(csv_path)
@@ -24,7 +26,10 @@ class RestaurantRecommender:
             user_input = input().lower()
             trigger = self._input_to_trigger(user_input=user_input)
             if trigger == "inform":
-                matched_preferences, levenshtein_used = keyword_match(user_input)
+                (
+                    matched_preferences,
+                    levenshtein_used,
+                ) = self.keyword_matcher.match_keyword(user_input)
                 self.preferences += matched_preferences
                 self.state_manager.current_preferences = self.preferences
 
@@ -35,7 +40,7 @@ class RestaurantRecommender:
 
                     if self.preferences.is_full():
                         self._find_restaurants()
-                        trigger = "restaurant_recommend"
+                        trigger = "preferences_filled"
 
             self.logger.debug(f"{trigger=}")
 
@@ -63,6 +68,13 @@ class RestaurantRecommender:
             DialogHandler.request_missing_info(
                 missing_keyword=self.preferences.missing_preferences()[0]
             )
+        elif self.state_manager.state == "suggest_restaurant":
+            DialogHandler.suggest_restaurant(
+                restaurant=next(self.recommend_restaurants),
+                price_range=self.preferences.price_range,
+                area=self.preferences.aress,
+                food_type=self.preferences.food_type,
+            )
         else:
             func = getattr(DialogHandler, self.state_manager.state)
             func()
@@ -71,7 +83,3 @@ class RestaurantRecommender:
         self.recommend_restaurants = self.restaurants.query(
             self.preferences.to_pandas_query()
         )
-
-
-def keyword_match(input_text) -> Tuple[Preferences, bool]:
-    return Preferences(), False
